10e1dcad436fb26f25ffda78d63a7af0d54c0bb7
Abhishek <abhishekyadavfeb1498@gmail.com>
2020-10-04 05:31:36 +0530

    finalCommit

--- src/Components/Itemadded.js
+++ src/Components/Itemadded.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import TaskList from "./TaskList";
+import "./Itemadded.css";
 
 const Itemadded = () => {
   const [task, setTask] = useState("");
@@ -8,25 +9,51 @@ const Itemadded = () => {
     setTask(event.target.value);
   };
   const addTask = () => {
-    setTaskadd((prevValue) => {
-      return [...prevValue, task];
+    if (task === "") {
+      return alert("please enter task");
+    } else {
+      setTaskadd((prevValue) => {
+        return [...prevValue, task];
+      });
+      setTask("");
+    }
+  };
+  const onDelete = (id) => {
+    setTaskadd((prevArr) => {
+      return prevArr.filter((cval, index) => {
+        return index !== id;
+      });
     });
-    setTask("");
   };
 
   return (
-    <>
-      <input
-        type="text"
-        placeholder="Enter the task to be accomplished"
-        onChange={takeInput}
-        value={task}
-      ></input>
-      <button onClick={addTask}>+</button>
+    <div className="main_div">
+      <span className="heading">
+        <h1>Your To Do list</h1>
+      </span>
+      <div className="input">
+        <input
+          type="text"
+          placeholder="Enter the task "
+          onChange={takeInput}
+          value={task}
+        ></input>
+        <button className="button_add" onClick={addTask}>
+          +
+        </button>
+      </div>
+      <h5>Your List</h5>
       {taskadd.map((cval, index) => {
-        return <TaskList key={index} tasks={cval} />;
+        return (
+          <TaskList
+            key={index}
+            id={index}
+            tasks={cval}
+            onDecrement={onDelete}
+          />
+        );
       })}
-    </>
+    </div>
   );
 };
 
--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,13 +1,22 @@
 import React from "react";
+import "./TaskList.css";
 
 const TaskList = (props) => {
   console.log(props);
   return (
-    <>
+    <div className="task_list">
       <ul>
         <li>{props.tasks}</li>
+        <button
+          className="delete"
+          onClick={() => {
+            props.onDecrement(props.id);
+          }}
+        >
+          -
+        </button>
       </ul>
-    </>
+    </div>
   );
 };
 
